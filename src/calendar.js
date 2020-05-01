70cfe834e2b0fe4ac95687f7cf72e57d58813502
mbyk <marubayashi@gmail.com>
2020-05-01 17:53:23 +0900

    日付データ構造の配列を追加

--- src/calendar.js
+++ src/calendar.js
@@ -1,16 +1,38 @@
 class Calendar {
   static WEEK_HEADER = '日 月 火 水 木 金 土'
+  static DAY_LEFT_PADDING = 2
   show() {
     const year = 2020
     const month = 5
-    return `     ${String(month).padStart(2, ' ')}月 ${year}      
+    const days = this.createDays()
+    const dayContent = this.convertToContent(days)
+
+    return `     ${String(month).padStart(
+      Calendar.DAY_LEFT_PADDING,
+      ' '
+    )}月 ${year}      
 ${Calendar.WEEK_HEADER}
-                1  2
- 3  4  5  6  7  8  9
-10 11 12 13 14 15 16
-17 18 19 20 21 22 23
-24 25 26 27 28 29 30
-31                  `
+${dayContent}`
+  }
+
+  createDays() {
+    return [
+      ['  ', '  ', '  ', '  ', '  ', ' 1', ' 2'],
+      [' 3', ' 4', ' 5', ' 6', ' 7', ' 8', ' 9'],
+      ['10', '11', '12', '13', '14', '15', '16'],
+      ['17', '18', '19', '20', '21', '22', '23'],
+      ['24', '25', '26', '27', '28', '29', '30'],
+      ['31', '  ', '  ', '  ', '  ', '  ', '  ']
+    ]
+  }
+
+  convertToContent(days) {
+    return days
+      .map((day) => {
+        const week = day.join(' ')
+        return week
+      })
+      .join('\n')
   }
 }
 
