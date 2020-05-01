0bd0ccbafaa4f89b06f810ac7f7996f9c9239fec
mbyk <marubayashi@gmail.com>
2020-05-01 20:32:53 +0900

    1970年1月と2100年12月のテストケースを追加

--- src/calendar.test.js
+++ src/calendar.test.js
@@ -36,3 +36,27 @@ test('show 2020/06 calendar', () => {
 28 29 30            `
   expect(cal.show()).toBe(expectValue)
 })
+
+test('show 1970/01 calendar', () => {
+  const cal = new Calendar(1970, 1)
+  const expectValue = `      1月 1970      
+日 月 火 水 木 金 土
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28 29 30 31`
+  expect(cal.show()).toBe(expectValue)
+})
+
+test('show 2020/11 calendar', () => {
+  const cal = new Calendar(2100, 12)
+  const expectValue = `     12月 2100      
+日 月 火 水 木 金 土
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29 30 31   `
+  expect(cal.show()).toBe(expectValue)
+})
